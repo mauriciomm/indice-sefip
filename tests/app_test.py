b3f356014864dcc07f01025605d6e8eebbbc7a78
Mauricio Moreira <mauricio.moreira@salvador.ba.gov.br>
2017-05-31 11:14:52 -0300

    WIP - Erro ao importar modulo app.

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,12 +1,13 @@
 import os
+import app
 import unittest
 import tempfile
 
 class AppTestCase(unittest.TestCase):
 
 	def test_main(self):
-		rv = self.app.get('/a')
-		assert b'No entries here so far' in rv.data
+		rv = self.app.get('/')
+		self.assertEqual(rv.data, "Home")
 
 if __name__ == '__main__':
     unittest.main()
